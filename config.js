ef525888cd9a25b20ad4a3546b35b5b55cac5127
vuhuuson220803 <104509275+vuhuuson220803@users.noreply.github.com>
2024-03-03 21:14:32 +0700

    123

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi Vân';
+const contentCard = 'Chúc Vân 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Vân trong hôm nay và cả những ngày sau.Hãy cười thật nhiều lên nhé! Không biết sao từ lúc nào mà mỗi ngày đều muốn gặp thấy bạn vui tươi và mình cảm thấy rất rất hạnh phúc vì bạn đã đến! Cảm ơn nhìu nhé! ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'van.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
